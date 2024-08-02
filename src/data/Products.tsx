0181130a0d7303cf2905313efa656fdb1e53e1ad
Niloufar <nilooooofar97@gmail.com>
2024-08-02 09:23:21 +0200

    add my data

---
+++ src/data/Products.tsx
@@ -0,0 +1,92 @@
+import AloeGellyImg from "../../public/images/Products/aloeGelly.jpg";
+import AlphaEImg from "../../public/images/Products/alpha-e-factor.jpg";
+import ArcticSeaImg from "../../public/images/Products/Arctic-sea.jpg";
+import BodyLotionImg from "../../public/images/Products/bodyLotion.webp";
+import BodyWashImg from "../../public/images/Products/bodyWash.webp";
+import DailyImg from "../../public/images/Products/daily.jpg";
+import DeodorantImg from "../../public/images/Products/deodorant.jpg";
+import KidsImg from "../../public/images/Products/kids.jpg";
+import MoveImg from "../../public/images/Products/move.jpg";
+import SunscreenImg from "../../public/images/Products/sunscreen.jpg";
+import TonerImg from "../../public/images/Products/toner.jpg";
+
+
+export const Products = [
+    {
+        id: 1,
+        Name : "Forever Aloe Vera Gelly",
+        category: "Skin Care",
+        price: 169.81,
+        imgUrL : AloeGellyImg
+    },
+    {
+        id: 2,
+        Name : "Alpha-E Factor",
+        category: "Skin Care",
+        price : 376.45,
+        imgUrL: AlphaEImg
+    },
+    {
+        id: 3,
+        Name : "Forever Arctic Sea",
+        category: "Nutritional",
+        price: 313.04,
+        imgUrL: ArcticSeaImg
+    },
+    {
+        id: 4,
+        Name : "Forever Aloe Body Lotion",
+        category: "Skin Care",
+        price: 213.91,
+        imgUrl: BodyLotionImg
+    },
+    {
+        id: 5,
+        Name : "Forever Aloe Body Wash",
+        category: "Skin Care",
+        price: 211.59,
+        imgUrl: BodyWashImg
+    },
+    {
+        id: 6,
+        Name : "Forever Daily",
+        category: "Nutritional",
+        price: 198.06,
+        imgUrL: DailyImg
+    },
+    {
+        id: 7,
+        Name : "Aloe Ever-Shield Deodorant Stick",
+        category: "Personal",
+        price: 81.78,
+        imgUrL: DeodorantImg
+    },
+    {
+        id: 8,
+        Name : "Forever Kids",
+        category: "Nutritional",
+        price: 124.03,
+        imgUrL: KidsImg
+    },
+    {
+        id: 8,
+        Name : "Forever Move",
+        category: "Nutritional",
+        price: 586.54,
+        imgUrL: MoveImg
+    },
+    {
+        id: 9,
+        Name : "Aloe Sunscreen",
+        category: "Skin Care",
+        price: 195.18,
+        imgUrl: SunscreenImg
+    },
+    {
+        id: 10,
+        Name : "Balancing Toner",
+        category: "Skin Care",
+        price: 204.13,
+        imgUrl: TonerImg
+    },
+]
