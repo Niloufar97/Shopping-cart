b82af0edabb52929b0fe32987a8e73791af94e79
Niloufar <nilooooofar97@gmail.com>
2024-08-12 11:27:15 +0200

    change style

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -9,9 +9,9 @@ type StoreItemType = {
 };
 
 export function StoreItem({ name, price, imgUrl }: StoreItemType) {
-  const quantity: number = 0;
+  const quantity: number = 1;
   return (
-    <Card className="h-100 p-2" >
+    <Card className="h-100 p-2">
       <Card.Img variant="top" src={imgUrl} style={{ objectFit: "cover" }} />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
@@ -20,8 +20,24 @@ export function StoreItem({ name, price, imgUrl }: StoreItemType) {
         </Card.Title>
         <div className="mt-auto mb-3">
           {quantity === 0 ? (
-            <Button className="bg-white text-dark border border-dark w-100">+ Add to cart</Button>
-          ) : null}
+            <Button className="bg-white text-dark border border-dark w-100" >
+              + Add to cart
+            </Button>
+          ) : (
+            <div className="d-flex flex-column gap-2 justify-content-center align-items-center">
+              <div className="d-flex gap-3 justify-content-center align-items-center ">
+                <Button variant="outline-primary">+</Button>
+                <div>
+                  <span style={{fontSize: "1.5rem"}}>{quantity} </span>
+                  in cart
+                </div>
+                <Button variant="outline-primary">-</Button>
+              </div>
+              {/* <Button variant="outline-danger" >
+                Remove
+              </Button> */}
+            </div>
+          )}
         </div>
       </Card.Body>
     </Card>
